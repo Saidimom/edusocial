643655ce151a816af097b090e210e4ce8cc5dee9
Saidimom Mukhamadiev <sm.mukhamadiev@gmail.com>
2025-11-25 21:35:08 +0500

    feat: simplfying main.dart

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EduSocial',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: null,
+    );
+  }
+}
